c072ae19b72fd204be5bbd9e270ae42d06aa7307
1766926408 <>
2026-04-16 19:21:37 +0800

    fix: remove InviteTracker from __init__.py to break google_sheet chain import

--- 项目管理/P-001_代码模板/modules/__init__.py
+++ 项目管理/P-001_代码模板/modules/__init__.py
@@ -3,14 +3,12 @@
 P-001 邀测招募自动化模块包
 """
 
-from .sqlite_manager import SQLiteManager as SheetManager
+from .sqlite_manager import SQLiteManager
 from .rewards import RewardCalculator
 from .analytics import AnalyticsEngine
-from .invite_tracker import InviteTracker
 
 __all__ = [
-    'SheetManager',
+    'SQLiteManager',
     'RewardCalculator',
     'AnalyticsEngine',
-    'InviteTracker'
 ]
